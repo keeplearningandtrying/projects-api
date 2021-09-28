ab294f1253111f1048e4f0ea438b7800a8d3a4d7
Brian Clozel <bclozel@vmware.com>
2021-09-28 18:40:46 +0200

    Expose Contributor and GitHub Profile information
    This commit uses the newly introduced GitHub client to fetch information
    about contributors and their GitHub profiles.
    
    For that, we need to implement the relationship between `Project` and
    `Contributor` using `@SchemaMapping`. We could apply the same reasoning
    for the `Contributor` to `Profile` relationship, but in this case we
    could optimize for batch loading. While this is not the case yet, as
    we're using GitHub's REST API, this setup could be still useful if the
    same contributor is fetched multiple times in the same request, or if we
    use GitHub's GraphQL API in the future.
    
    We can test this new feature with the following query:
    
    ```
    {
      project(slug: "spring-graphql") {
        name
        status
        contributors {
          gitHubId
          profile {
            name
          }
        }
      }
    }
    ```

--- src/main/java/io/spring/sample/projects/ProjectsController.java
+++ src/main/java/io/spring/sample/projects/ProjectsController.java
@@ -1,14 +1,22 @@
 package io.spring.sample.projects;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import io.spring.sample.projects.contributors.Contributor;
+import io.spring.sample.projects.contributors.GitHubClient;
+import io.spring.sample.projects.contributors.Profile;
 import io.spring.sample.projects.metadata.Project;
 import io.spring.sample.projects.metadata.Projects;
 import io.spring.sample.projects.metadata.Release;
 import io.spring.sample.projects.repository.ArtifactRepositories;
 import io.spring.sample.projects.repository.ArtifactRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -20,9 +28,12 @@ public class ProjectsController {
 
 	private final ArtifactRepositories artifactRepositories;
 
-	public ProjectsController(Projects projects, ArtifactRepositories artifactRepositories) {
+	private final GitHubClient gitHubClient;
+
+	public ProjectsController(Projects projects, ArtifactRepositories artifactRepositories, GitHubClient gitHubClient) {
 		this.projects = projects;
 		this.artifactRepositories = artifactRepositories;
+		this.gitHubClient = gitHubClient;
 	}
 
 	@QueryMapping
@@ -37,5 +48,15 @@ public class ProjectsController {
 			case MILESTONE -> this.artifactRepositories.findById("spring-milestones");
 			case GENERAL_AVAILABILITY -> this.artifactRepositories.findById("spring-releases");
 		};
+	}
+
+	@SchemaMapping
+	public Flux<Contributor> contributors(Project project) {
+		return this.gitHubClient.getContributors(project.getSlug());
+	}
+
+	@BatchMapping
+	public Mono<Map<Contributor, Profile>> profile(List<Contributor> contributors) {
+		return this.gitHubClient.fetchProfiles(Flux.fromIterable(contributors));
 	}
 }
