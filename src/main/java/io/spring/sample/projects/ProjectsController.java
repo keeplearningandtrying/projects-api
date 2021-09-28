aee769d6bc52ced29510bca906f3c1e91f4e91a5
Brian Clozel <bclozel@vmware.com>
2021-09-28 17:29:23 +0200

    Link ArtifactRepository to Releases
    Because Project Releases are hosted in artifact repositories, we could
    improve our object graph and link releases to their respective artifact
    repository.
    
    This commit expresses the relationship between a Release and its
    Repository using a `@SchemaMapping` annotation.
    The method signature gives enough information to the engine for
    materializing that relationship.
    
    We can now request the repository information in our previous query:
    
    ```
    {
      project(slug: "spring-graphql") {
        name
        status
        releases {
          version
          status
          repository {
            url
          }
        }
      }
    }
    ```

--- src/main/java/io/spring/sample/projects/ProjectsController.java
+++ src/main/java/io/spring/sample/projects/ProjectsController.java
@@ -4,9 +4,13 @@ import java.util.Optional;
 
 import io.spring.sample.projects.metadata.Project;
 import io.spring.sample.projects.metadata.Projects;
+import io.spring.sample.projects.metadata.Release;
+import io.spring.sample.projects.repository.ArtifactRepositories;
+import io.spring.sample.projects.repository.ArtifactRepository;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,12 +18,24 @@ public class ProjectsController {
 
 	private final Projects projects;
 
-	public ProjectsController(Projects projects) {
+	private final ArtifactRepositories artifactRepositories;
+
+	public ProjectsController(Projects projects, ArtifactRepositories artifactRepositories) {
 		this.projects = projects;
+		this.artifactRepositories = artifactRepositories;
 	}
 
 	@QueryMapping
 	public Optional<Project> project(@Argument String slug) {
 		return this.projects.findBySlug(slug);
+	}
+
+	@SchemaMapping
+	public Optional<ArtifactRepository> repository(Release release) {
+		return switch (release.getStatus()) {
+			case SNAPSHOT -> this.artifactRepositories.findById("spring-snapshots");
+			case MILESTONE -> this.artifactRepositories.findById("spring-milestones");
+			case GENERAL_AVAILABILITY -> this.artifactRepositories.findById("spring-releases");
+		};
 	}
 }
